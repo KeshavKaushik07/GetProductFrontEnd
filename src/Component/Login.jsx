63113c98f095a58469d42899a02a247f4f09af76
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-01-27 19:01:00 +0530

    add forgot password page

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -6,44 +6,47 @@ const Login = ({ setShowLogin, setShowShowSignUp, setUserData, Api }) => {
     email: "",
     password: "",
   });
+  const [forgotInput, setForgotInput] = useState({
+    email: "",
+    answer: "",
+    password: ""
+  })
 
-  const [showPassword,setShowPassword] = useState("false");
-  // const [success,setSuccess] = useState(false);
-  // const [error,setError] = useState(false);
-const [showSpinner, setShowSpinner] = useState(false);
+  const [showPassword, setShowPassword] = useState("false");
+  const [showForgot, setShowForgot] = useState(false);
+  const [showForPass, setShowForPass] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
 
- async function handleLogin(){
-    try{
+
+  async function handleLogin() {
+    try {
       setShowSpinner(true);
-      const apiData = await fetch(`${Api}/auth/login`,{
+      const apiData = await fetch(`${Api}/auth/login`, {
         method: "POST",
-        headers : {"Content-Type": "application/json"},
-        body : JSON.stringify({...input})
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...input })
       });
       setShowSpinner(false);
       const data = await apiData.json();
       // console.log(apiData);
       // console.log(data);
-      if(data.success)
-      {
+      if (data.success) {
         setInput({
           email: "",
-    password: "",
+          password: "",
         });
         // setSuccess(true);
         setUserData(data.user);
-         setShowLogin(false);
+        setShowLogin(false);
         //  setError(false);
-         alert(data.message);
-      }else
-        {
+        alert(data.message);
+      } else {
         // setError(data.message);
         alert(data.message);
         // setError(false);
       }
-    }catch(err)
-    {
-      console.log("error in login :" ,err)
+    } catch (err) {
+      console.log("error in login :", err)
     }
   }
 
@@ -52,33 +55,86 @@ const [showSpinner, setShowSpinner] = useState(false);
   return (
     <>
       <div className={style.container}>
-        <div className={style.loginForm}>
-          <h1 className={style.title}>Login</h1>
-          <div className={style.form}>
-            <button onClick={() => setShowLogin(false)} className={style.Cross}>X</button>
-            <label htmlFor="email">Email : </label>
-            <input type="email" onChange={(e) => setInput({ ...input, email: e.target.value })} placeholder='Enter Email' name='email'/>
-            <label htmlFor="password">Password : </label>
-            <input type={showPassword ? "password":"text"} onChange={(e) => setInput({ ...input, password: e.target.value })} placeholder='Enter password' name='password'/>
-            <div className={style.check}>
-               <input type="checkbox" name="" id="" onChange={()=>setShowPassword(!showPassword)}/>
-            <span>Show Password</span>
+        {!showForgot ?
+          <div className={style.loginForm}>
+            <h1 className={style.title}>Login</h1>
+            <div className={style.form}>
+              <button onClick={() => setShowLogin(false)} className={style.Cross}>X</button>
+              <label htmlFor="email">Email : </label>
+              <input type="email" onChange={(e) => setInput({ ...input, email: e.target.value })} placeholder='Enter Email' name='email' />
+              <label htmlFor="password">Password : </label>
+              <input type={showPassword ? "password" : "text"} onChange={(e) => setInput({ ...input, password: e.target.value })} placeholder='Enter password' name='password' />
+              <div className={style.check}>
+                <input type="checkbox" name="" id="" onChange={() => setShowPassword(!showPassword)} />
+                <span>Show Password</span>
+              </div>
+
+              <div className={style.signup}>
+                <span>New user ? </span>
+                <button className={style.signButton} onClick={() => { setShowLogin(false); setShowShowSignUp(true) }}>sing up</button>
+              </div>
+
+              <button className={style.loginButton} onClick={handleLogin}>Login</button>
+
+              <p className='text-lg self-center'>forgot Password :
+                <button
+                  className='underline text-xl text-blue-800'
+                  onClick={() => { setShowForgot(true) }}
+                >forgot</button></p>
             </div>
 
-            <div className={style.signup}>
-              <span>New user ? </span>
-              <button className={style.signButton} onClick={()=> {setShowLogin(false); setShowShowSignUp(true )}}>sing up</button>
-              </div>
-           
-            <button className={style.loginButton} onClick={handleLogin}>Login</button>
           </div>
+          :
+          <div className={style.loginForm}>
+            <h1 className={style.title}>Reset password</h1>
+            <form action="">
+              <div className={style.form}>
+                <button onClick={() => setShowLogin(false)} className={style.Cross}>X</button>
+                <label htmlFor="email">Email : </label>
+                <input
+                  type="email"
+                  onChange={(e) => setForgotInput({ ...forgotInput, email: e.target.value })}
+                  placeholder='Enter Email'
+                  required />
+                <label htmlFor="answer">Asnwer : </label>
+                <input
+                  type="text"
+                  required
+                  onChange={(e) => setForgotInput({ ...forgotInput, answer: e.target.value })}
+                  placeholder='Enter Answer '
+                  name='answer' />
+                <label htmlFor="password">Password : </label>
+                <input
+                  type={!showForPass ? "password" : "text"}
+                  required
+                  onChange={(e) => setForgotInput({ ...forgotInput,password: e.target.value })}
+                  placeholder='Enter password'
+                  name='password' />
+                <label htmlFor="password">Conform password : </label>
+                <input
+                  type={!showForPass ? "password" : "text"}
+                  required
+                  onChange={(e) => setInput(console.log("bb"))}
+                  placeholder='Re-enter the password'
+                  name='password' />
+                <div className={style.check}>
+                  <input type="checkbox" name="" id="" onChange={() => setShowForPass(!showForPass)} />
+                  <span>Show Password</span>
+                </div>
+                <button type="submit"
+                  className="bg-amber-600 w-full self-center py-2 px-3 rounded text-white mt-2 font-bold"
+                >Update</button>
+              </div>
+            </form>
+            <button className="bg-blue-700 w-fit self-center py-1 px-3 rounded text-white" onClick={() => { setShowForgot(false) }}>back to login</button>
+          </div>
+        }
 
-        </div>
         {
-                    showSpinner &&
-                 <div className="w-15 h-15 border-8 border-blue-500 border-t-transparent rounded-full motion-safe:animate-spin absolute"></div>
+          showSpinner &&
+          <div className="w-15 h-15 border-8 border-blue-500 border-t-transparent rounded-full motion-safe:animate-spin absolute"></div>
 
-                }
+        }
       </div>
     </>
   )
